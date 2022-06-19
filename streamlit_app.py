fd4a1cc0688559ad39dc092b8144e283b5dbf9af
mandarc64 <cmandar0604@gmail.com>
2022-06-19 20:26:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,17 @@
 # from gingerit.gingerit import GingerIt
 import streamlit as st
+import sys
+import os
 
 st.title('Spelling & Grammar Checker')
 text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
 
+#Added path to sys.path
+MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(MODULE_DIR))
+
 #relative import 
-from .local.gingerit import GingerIt 
+from local.gingerit import GingerIt 
 
 #from gingerit.gingerit import GingerIt
 parser = GingerIt()
